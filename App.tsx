d7ae0cb5d38b3ef6abea3baf45326c36a3a0686d
onofre2 <jrferreiraa22@gmail.com>
2026-09-05 09:47:35 -0300

    fix: adiciona SafeAreaProvider que faltava e causava crash na inicializacao

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 import { initDatabase } from './src/services/database';
 import { PacienteAtivoProvider } from './src/context/PacienteAtivoContext';
@@ -12,11 +13,13 @@ export default function App() {
   }, []);
 
   return (
-    <PacienteAtivoProvider>
-      <NavigationContainer>
-        <StatusBar style="light" />
-        <AppNavigator />
-      </NavigationContainer>
-    </PacienteAtivoProvider>
+    <SafeAreaProvider>
+      <PacienteAtivoProvider>
+        <NavigationContainer>
+          <StatusBar style="light" />
+          <AppNavigator />
+        </NavigationContainer>
+      </PacienteAtivoProvider>
+    </SafeAreaProvider>
   );
 }
