ef2ddfe11025886ba78810b1a5567bbd6720d282
onofre2 <jrferreiraa22@gmail.com>
2026-09-19 13:46:24 -0300

    feat: referencias cientificas do joelho e do retrope no PDF

--- src/services/pdfService.ts
+++ src/services/pdfService.ts
@@ -213,6 +213,11 @@ function paginaReferencias(): string {
       <li><b>SAPO</b> (Duarte et al., 2005) - Software para Avaliacao Postural.</li>
       <li><b>POTSI / ATSI</b> (Suzuki et al., 1999) - Indices de simetria do tronco.</li>
       <li><b>Angulo Craniovertebral</b> - posicionamento anterior da cabeca.</li>
+      <li><b>Angulo do Retrope</b> (Eng e Pierrynowski, 1994) - varo abaixo de 0 graus, normal entre 0 e 6, valgo acima de 6. Faixas de transicao de 3 graus sao criterio operacional deste aplicativo.</li>
+      <li><b>Protocolo de quatro marcadores do retrope</b> - base do calcaneo, insercao do tendao de Aquiles, tendao na altura do maleolo medial e centro da perna 15 cm acima.</li>
+      <li><b>Alinhamento frontal e sagital do joelho</b> - angulo no vertice com produto vetorial 2D. Faixas de classificacao sao criterio operacional deste aplicativo.</li>
+      <li><b>SAPO, vista posterior</b> - ponto medial da perna, linha intermaleolar e tendao calcaneo bilateralmente.</li>
+      <li><b>Lordose cervical</b> (SAPO) - trago e C7 com vertice no acromio. Medida de superficie, validada contra goniometria, nao contra radiografia.</li>
       <li><b>Goniometria de Kendall / AAOS</b> - amplitudes articulares.</li>
       <li><b>Ciclo da Marcha de Perry</b> - angulos por fase da marcha.</li>
     </ul>
